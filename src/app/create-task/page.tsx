6ccc78f1aa5ec17daa91bc0477649adb6bac9cf8
josé <jg345762@gmail.com>
2023-10-23 13:43:46 -0300

    taskCard updated

--- src/app/create-task/page.tsx
+++ src/app/create-task/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { FormEvent, useEffect } from "react";
 import { useAppContext } from "@/context/AppContext";
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { useAppContext } from "@/context/AppContext";
 import { Task } from "@/ts/interfaces";
